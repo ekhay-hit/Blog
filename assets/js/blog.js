896648f4f62d0ac3d59e6571eb9037e9b77a7ef7
Houssine <ekhay1@outlook.com>
2024-05-06 18:34:17 -0400

    Worked on resolving issue with pushing blog to the blog html

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,28 +1,29 @@
 
-const parent = document.querySelector("body");
+// const parent = document.querySelector("body");
 const section = document.querySelector(".section");
 const divContainer = document.querySelector(".div-container");
 
 
-for(let i=0; i<2;i++){
+for(let i=0; i<3;i++){
+
 const newDiv = document.createElement('div') ;
 newDiv.setAttribute("class", "div-element");
+divContainer.appendChild(newDiv);
+
 const title=document.createElement('h3');
 title.innerHTML="Blog Title";
 newDiv.appendChild(title);
 
 const content= document.createElement('p');
-// content.setAttribute("class", "blog-content");
-content.innerHTML="This is first b log that I am writing. It is good to test way";
+content.setAttribute("class", "blog-content");
+content.innerHTML="This is first b log that I am writing.";
 newDiv.appendChild(content);
 
 const author = document.createElement('span');
 author.innerHTML= "create by: Houssine Khay";
-// author.setAttribute("class", "blog-author");
+author.setAttribute("class", "blog-author");
 newDiv.appendChild(author);
-divContainer.appendChild(newDiv);
 
 
-document.body.appendChild(divContainer);
 }
 
