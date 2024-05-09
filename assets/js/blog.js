9ca7ae969d1d74e4b803629b488e1f745b70a570
Khay <elhoussine.khay2@orlandohealth.com>
2024-05-09 09:12:51 -0400

    Added comment to all pages , and updated the blog section with a minmum width

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,49 +4,49 @@ const section = document.querySelector(".section");
 const divContainer = document.querySelector(".div-container");
 const modeSwitcher = document.querySelector(".modeSwitcher");
 const body= document.querySelector("#bodyId");
-
+// set mode to light class
 let mode ='light';
-
+// event listner for switch mode button
 modeSwitcher.addEventListener('click', function(){
+  // check if mode is dark
     if (mode === 'dark') {
         mode = 'light';
+        // set attrivute to class light
         body.setAttribute('class', 'light');
       }
       // If mode is light, apply dark background
       else {
         mode = 'dark';
+        //if mode is ligh set attribute to dark
         body.setAttribute('class', 'dark');
       }
 });
 
-
+// get blogArray from local storage and assign it to arrBlog
 const arrBlog = JSON.parse(localStorage.getItem("blogArry"));
-console.log(arrBlog);
+// check if local storage not impty, if not proceed
 if(arrBlog !== null){
-
-// console.log(arrBlog);
-
+// loop to itirate through arrBlog
 for(let i=0; i<arrBlog.length;i++){
-
+// create div and set attribute to the append it to divContainer
 const newDiv = document.createElement('div') ;
 newDiv.setAttribute("class", "div-element");
 divContainer.appendChild(newDiv);
-
+// create header 3 add title to it and append it newDiv
 const title=document.createElement('h3');
 title.setAttribute("class", "blog-title");
 title.innerHTML=arrBlog[i].title;
 newDiv.appendChild(title);
-
+// create <p> add content to it and append it newDiv
 const content= document.createElement('p');
 content.setAttribute("class", "blog-content");
 content.innerHTML=arrBlog[i].content;
 newDiv.appendChild(content);
-
+// create <span> add author to it and append it newDiv
 const author = document.createElement('span');
 author.innerHTML= arrBlog[i].userName;
 author.setAttribute("class", "blog-author");
 newDiv.appendChild(author);
-
 }
 }
 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,60 +6,72 @@ const submitBtn = document.querySelector("#btn");
 
 // selector for backgroun theme selector
 const modeSwitcher = document.querySelector(".modeSwitcher");
+// selector for the body
 const body = document.querySelector("#bodyId");
+// set mode to light
 let mode = "light";
+// create a varaible 
 let blogArry;
+// check if the varaible array has any values if not initialize an empty array
 if (localStorage.getItem("blogArry") === null) {
   blogArry = [];
+  // if there are items in local storage get items and assinged to the array
 } else {
   blogArry = JSON.parse(localStorage.getItem("blogArry"));
 }
+
+// function to story object of the blog
 function storeBlog() {
   localStorage.setItem("blogArry", JSON.stringify(blogArry));
 }
-
+ // even listner for form submit button
 submitBtn.addEventListener("click", function (e) {
+  // prevent the page to refresh
   e.preventDefault();
-  
+  // check if the input are not empty, if empty sent alert
   if ((userNameInput.value === "") || (titleInput.value === "") || (contentInput.value ==="")) {
    
     alert("Please complete the form");
   } else {
   
+    // if inputs are not empty get the value and assined them to the follwoing varaibles
     
-      console.log("I am here before get attribut");
     const users = userNameInput.value;
     const title = titleInput.value;
     const content = contentInput.value.trim();
-    //   console.log("I am here after get attribut");
-    //   console.log(`This is username ${users}`);
+    // create an object blog
     const blog = {
       userName: users,
       title: title,
       content: content,
     };
-
+    // push object to the array
     blogArry.push(blog);
-    console.log(blogArry);
-    //   localStorage.setItem('blogArry',JSON.stringify(blogArry));
+   
+    //   call the function to store the opject in local storage
     storeBlog();
+    // open windows blog 
     window.open("/blog.html");
+    // empty the form input 
     userNameInput.value = "";
     titleInput.value = "";
     contentInput.value = "";
   }
 });
 
-// implementation to back ground mode.
-
+// implementation to back-ground mode.
+// eventListner for the swithc mode button
 modeSwitcher.addEventListener("click", function () {
+  // check mode value and switch accordinally 
   if (mode === "dark") {
     mode = "light";
+    // set the attribute to light mode
     body.setAttribute("class", "light");
   }
   // If mode is light, apply dark background
   else {
     mode = "dark";
+    // set attribute to dark mode by adding class dark to body
     body.setAttribute("class", "dark");
   }
 });
