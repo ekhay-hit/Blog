3d0dd2a8af00853ef584e8968b929a44ccd0f0e0
Houssine <ekhay1@outlook.com>
2024-05-08 21:43:56 -0400

    added the funtionality of opening blog page after submiting the from in the index page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,52 +7,59 @@ const submitBtn = document.querySelector("#btn");
 // selector for backgroun theme selector
 const modeSwitcher = document.querySelector(".modeSwitcher");
 const body = document.querySelector("#bodyId");
-let mode = 'light';
+let mode = "light";
 let blogArry;
-if (localStorage.getItem("blogArry")=== null){
-      blogArry=[];
-}else{
-  blogArry=JSON.parse(localStorage.getItem("blogArry"));
+if (localStorage.getItem("blogArry") === null) {
+  blogArry = [];
+} else {
+  blogArry = JSON.parse(localStorage.getItem("blogArry"));
 }
-function storeBlog(){
-    localStorage.setItem('blogArry',JSON.stringify(blogArry));
-};
+function storeBlog() {
+  localStorage.setItem("blogArry", JSON.stringify(blogArry));
+}
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
-//   console.log("I am here before get attribut");
-  const users = userNameInput.value;
-  const title = titleInput.value;
-  const content = contentInput.value.trim();
-//   console.log("I am here after get attribut");
-//   console.log(`This is username ${users}`);
-  const blog = {
-    userName: users,
-    title: title,
-    content: content,
-  };
+  
+  if ((userNameInput.value === "") || (titleInput.value === "") || (contentInput.value ==="")) {
+   
+    alert("Please complete the form");
+  } else {
+  
+    
+      console.log("I am here before get attribut");
+    const users = userNameInput.value;
+    const title = titleInput.value;
+    const content = contentInput.value.trim();
+    //   console.log("I am here after get attribut");
+    //   console.log(`This is username ${users}`);
+    const blog = {
+      userName: users,
+      title: title,
+      content: content,
+    };
 
-  blogArry.push(blog);
-  console.log(blogArry);
-//   localStorage.setItem('blogArry',JSON.stringify(blogArry));
-storeBlog();
-  userNameInput.value ='';
-  titleInput.value ='';
-  contentInput.value = '';
+    blogArry.push(blog);
+    console.log(blogArry);
+    //   localStorage.setItem('blogArry',JSON.stringify(blogArry));
+    storeBlog();
+    window.open("/blog.html");
+    userNameInput.value = "";
+    titleInput.value = "";
+    contentInput.value = "";
+  }
 });
-
 
 // implementation to back ground mode.
 
-modeSwitcher.addEventListener('click', function(){
-    if (mode === 'dark') {
-        mode = 'light';
-        body.setAttribute('class', 'light');
-      }
-      // If mode is light, apply dark background
-      else {
-        mode = 'dark';
-        body.setAttribute('class', 'dark');
-      }
-})
-
+modeSwitcher.addEventListener("click", function () {
+  if (mode === "dark") {
+    mode = "light";
+    body.setAttribute("class", "light");
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = "dark";
+    body.setAttribute("class", "dark");
+  }
+});
