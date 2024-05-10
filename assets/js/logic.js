f3e5d9bf19443e01cfff6a7025b169da07d51665
Houssine <ekhay1@outlook.com>
2024-05-09 20:54:41 -0400

    upadated the link to open blog page when clicked submit form

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,7 +10,7 @@ const modeSwitcher = document.querySelector(".modeSwitcher");
 const body = document.querySelector("#bodyId");
 // set mode to light
 let mode = "light";
-// create a varaible 
+// create a varaible
 let blogArry;
 // check if the varaible array has any values if not initialize an empty array
 if (localStorage.getItem("blogArry") === null) {
@@ -24,18 +24,21 @@ if (localStorage.getItem("blogArry") === null) {
 function storeBlog() {
   localStorage.setItem("blogArry", JSON.stringify(blogArry));
 }
- // even listner for form submit button
+// even listner for form submit button
 submitBtn.addEventListener("click", function (e) {
   // prevent the page to refresh
   e.preventDefault();
   // check if the input are not empty, if empty sent alert
-  if ((userNameInput.value === "") || (titleInput.value === "") || (contentInput.value ==="")) {
-   
+  if (
+    userNameInput.value === "" ||
+    titleInput.value === "" ||
+    contentInput.value === ""
+  ) {
     alert("Please complete the form");
   } else {
-  
+    window.open("./blog.html");
     // if inputs are not empty get the value and assined them to the follwoing varaibles
-    
+
     const users = userNameInput.value;
     const title = titleInput.value;
     const content = contentInput.value.trim();
@@ -47,12 +50,12 @@ submitBtn.addEventListener("click", function (e) {
     };
     // push object to the array
     blogArry.push(blog);
-   
+
     //   call the function to store the opject in local storage
     storeBlog();
-    // open windows blog 
-    window.open("/blog.html");
-    // empty the form input 
+    // open windows blog
+
+    // empty the form input
     userNameInput.value = "";
     titleInput.value = "";
     contentInput.value = "";
@@ -62,7 +65,7 @@ submitBtn.addEventListener("click", function (e) {
 // implementation to back-ground mode.
 // eventListner for the swithc mode button
 modeSwitcher.addEventListener("click", function () {
-  // check mode value and switch accordinally 
+  // check mode value and switch accordinally
   if (mode === "dark") {
     mode = "light";
     // set the attribute to light mode
